c681505a2b71bc00731846764f54bd2752f2b425
Tarif Halabi <tarif.halabi@gmail.com>
2025-07-08 15:20:52 -0400

    Fix wind direction missing implying VRB.

--- src/app/pipe/wind-direction-pipe.ts
+++ src/app/pipe/wind-direction-pipe.ts
@@ -3,6 +3,9 @@ import { Pipe, PipeTransform } from '@angular/core';
 @Pipe({ name: 'windDirection' })
 export class WindDirectionPipe implements PipeTransform {
     transform(windDirDegrees: number): string {
+        if (!windDirDegrees) {
+            return 'VRB'
+        }
         let windDirection: string = "" + windDirDegrees;
         while (windDirection.length < 3) windDirection = "0" + windDirection;
         return windDirection;
